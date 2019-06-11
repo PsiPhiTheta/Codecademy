72b4ea4362db8b623c2de941d41faf2e94bc2870
Tom <thomashollis1@gmail.com>
2019-06-11 11:21:52 +0100

    part 6/11

--- SQL/Ex1.sql
+++ SQL/Ex1.sql
@@ -5,3 +5,16 @@ CREATE TABLE celebs (
    name TEXT, 
    age INTEGER
 ); 
+
+INSERT INTO celebs (id, name, age) 
+VALUES (1, 'Justin Bieber', 22);
+
+INSERT INTO celebs (id, name, age) 
+
+VALUES (2, 'Beyonce Knowles', 33); 
+
+INSERT INTO celebs (id, name, age) 
+VALUES (3, 'Jeremy Lin', 26); 
+
+INSERT INTO celebs (id, name, age) 
+VALUES (4, 'Taylor Swift', 26); 
